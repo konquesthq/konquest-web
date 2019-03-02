6bad2bed7e1640d4c6e1988dd15ddccdaa60b003
John Kerr <kerr.john@gmail.com>
2019-03-02 22:52:37 +0000

    gh-17 Large resource item layout

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,26 +1,57 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import {Link, graphql} from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import './resources.scss';
 
 const ResourceItem = ({resourceItem}) => (
-  <div>
-    <Link to={`/resources/${resourceItem.id}`}>{resourceItem.title}</Link>
+  <div className="resource-item">
+    <div className="image">&nbsp;</div>
+    <div className="details">
+      <h2>{resourceItem.title}</h2>
+      <p>Etiam in faucibus lectus, ut lobortis ex. Pellentese habitant morbi tristique senectus et netus. Prae interdum
+        sed sapien consequat tincidunt. </p>
+      <Link to={`/resources/${resourceItem.id}`}>Read More</Link>
+    </div>
   </div>
 );
 
 class ResourceListPage extends React.Component {
   render() {
     const resourceItems = this.props.data.allContentfulResourceItem.edges;
+    let extraResourceItems = null;
+    if (resourceItems.length > 3) {
+      extraResourceItems = (
+        <ul>
+          {resourceItems
+            .slice(4)
+            .map(({node}) => (
+              <li key={node.id}>
+                <ResourceItem resourceItem={node}/>
+              </li>
+            ))}
+        </ul>
+      );
+    }
     return (
-      <Layout>
+      <Layout className="resources">
         <SEO title="Resources" keywords={[`resources`, `blog`]}/>
-        <h1>Resource Items</h1>
-        <p>Resources will go here...</p>
-        {resourceItems.map(({node}, i) => (
-          <ResourceItem resourceItem={node} key={node.id} />
-        ))}
+        <h1>Resources</h1>
+        <div className="contents">
+          <div>
+            <ul>
+              {resourceItems
+                .slice(0, 3)
+                .map(({node}) => (
+                  <li key={node.id}>
+                    <ResourceItem resourceItem={node}/>
+                  </li>
+                ))}
+            </ul>
+            {extraResourceItems}
+          </div>
+        </div>
       </Layout>
     )
   }
@@ -30,7 +61,7 @@ export default ResourceListPage
 
 export const resourceQuery = graphql`
   {
-    allContentfulResourceItem {
+    allContentfulResourceItem(sort: { fields: [createdAt], order: [DESC]}) {
       edges {
         node {
           id,
