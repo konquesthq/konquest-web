d34f9c76116fbc33a4c37849ecf70941a794586f
John Kerr <kerr.john@gmail.com>
2019-03-02 20:17:12 +0000

    fixes gh-20 Thank You page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,14 +2,12 @@ import React from "react"
 import Info from "../components/info"
 import SEO from "../components/seo"
 
-import './404.scss';
-
-const NotFoundPage = () => (
-  <Info className="not-found">
-    <SEO title="404: Not found" />
-    <h1>This isn't the primate you're looking for</h1>
+const ThanksPage = () => (
+  <Info>
+    <SEO title="Thank You" />
+    <h1>Message Received, Thank You</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla volutpat eget.</p>
   </Info>
 );
 
-export default NotFoundPage;
+export default ThanksPage;
