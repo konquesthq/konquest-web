e3355a7d243bf6e8b46dd19dd2fc4623cf41e3ef
John Kerr <kerr.john@gmail.com>
2019-03-03 22:36:48 +0000

    gh-12 Home intro text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
-import React from "react"
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import "./index.scss";
 
 class IndexPage extends React.Component {
   render() {
     return (
-      <Layout>
+      <Layout className="home">
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
-        <h1>Commission. Sorted.</h1>
-        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
-          <Image />
+        <div className="contents">
+          <div className="intro">
+            <h1>Commission. Sorted.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla volutpat
+              eget. Duis non massa iaculis, euismod nunc quis, egestas metus. </p>
+            <button type="button">Btn Text</button>
+          </div>
         </div>
       </Layout>
     )
