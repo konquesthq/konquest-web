f498cdf76e0260bcb386f2aeee89f3b4181f16bc
John Kerr <kerr.john@gmail.com>
2019-03-03 21:12:59 +0000

    fixes gh-7 Added recent articles

--- src/templates/resource.js
+++ src/templates/resource.js
@@ -1,7 +1,11 @@
-import React from "react"
-import {graphql} from "gatsby"
-import * as PropTypes from "prop-types"
-import Layout from "../components/layout"
+import React from "react";
+import {graphql} from "gatsby";
+import * as PropTypes from "prop-types";
+import Layout from "../components/layout";
+import ShortResourceItem from "../components/resourceItem";
+import RequestDemo from "../components/requestDemo";
+
+import "./resource.scss";
 
 const propTypes = {
   data: PropTypes.object.isRequired,
@@ -9,15 +13,30 @@ const propTypes = {
 
 class ResourceItemTemplate extends React.Component {
   render() {
-    const resourceItem = this.props.data.contentfulResourceItem;
+    const resourceItem = this.props.data.item;
+    const recent = this.props.data.recent.edges;
     return (
-      <Layout>
+      <Layout className="resource-page">
         <h1>{resourceItem.title}</h1>
         <div
           dangerouslySetInnerHTML={{
             __html: resourceItem.content.childContentfulRichText.html,
           }}
         />
+        <div className="recent-articles">
+          <h2>Recent Articles</h2>
+          <ul className="resource-items">
+            {
+              recent
+                .map(({node}) => (
+                  <li key={node.id}>
+                    <ShortResourceItem resourceItem={node}/>
+                  </li>
+                ))}
+            }
+          </ul>
+        </div>
+        <RequestDemo/>
       </Layout>
     )
   }
@@ -29,11 +48,22 @@ export default ResourceItemTemplate;
 
 export const resourceItemQuery = graphql`
   query($id: String!) {
-    contentfulResourceItem(id: { eq: $id }) {
+    item: contentfulResourceItem(id: { eq: $id }) {
       title,
       content {
         childContentfulRichText {
           html
+        }
+      }
+    }
+    recent: allContentfulResourceItem(filter: { id: { ne: $id }}, sort: { fields: [createdAt], order: [DESC]}, limit: 3) {
+      edges {
+        node {
+          id,
+          title,
+          description {
+            description
+          }
         }
       }
     }
