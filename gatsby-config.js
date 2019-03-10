649e7aa48c079450f6ad7c9aba5bd21b64a70311
John Kerr <kerr.john@gmail.com>
2019-03-10 09:18:38 +0000

    gh-22 Added placholder description and other config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Konquest`,
+    description: `Take the pain out of payroll with auto-calculation of commissions, no matter how complex your scheme setup.`,
+    author: `@johnjkerr`,
   },
   pathPrefix: "/konquest-web",
   plugins: [
@@ -23,11 +23,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `konquest`,
+        short_name: `konquest`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#c01b1f`,
+        theme_color: `#c01b1f`,
         display: `minimal-ui`,
         icon: `src/images/favicon-96x96.png`, // This path is relative to the root of the site.
       },
