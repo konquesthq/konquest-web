2ed2167ebe9e6a103c729594d1c202ddf0e960a9
John Kerr <kerr.john@gmail.com>
2019-03-10 22:54:35 +0000

    gh-12 Added home nav layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,41 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import "./index.scss";
+
+const PersonaNavigationItem = ({title, children}) => (
+  <li>
+    <h3>{title}</h3>
+    {children}
+  </li>
+);
+
+PersonaNavigationItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+};
+
+const PersonaNavigation = () => (
+  <ul>
+    <PersonaNavigationItem title="Commercial"><p>Ut imperdiet sed felis vitae aliquam. Pellent posuere metus in
+      auctor.</p></PersonaNavigationItem>
+    <PersonaNavigationItem title="Operations"><p>Ut imperdiet sed felis vitae aliquam. Pellent posuere metus in
+      auctor.</p></PersonaNavigationItem>
+    <PersonaNavigationItem title="Finance"><p>Ut imperdiet sed felis vitae aliquam. Pellent posuere metus in auctor.</p>
+    </PersonaNavigationItem>
+    <PersonaNavigationItem title="Sales"><p>Ut imperdiet sed felis vitae aliquam. Pellent posuere metus in auctor.</p>
+    </PersonaNavigationItem>
+  </ul>
+);
+
+const HomeNavigation = () => (
+  <nav className="persona-navigation">
+    <h2>Choose your role or department</h2>
+    <PersonaNavigation/>
+  </nav>
+);
 
 class IndexPage extends React.Component {
   render() {
@@ -16,6 +49,7 @@ class IndexPage extends React.Component {
               eget. Duis non massa iaculis, euismod nunc quis, egestas metus. </p>
             <button type="button">Btn Text</button>
           </div>
+          <HomeNavigation/>
         </div>
       </Layout>
     )
