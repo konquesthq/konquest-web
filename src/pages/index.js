df0256da4f9a39c3d7a6f545d8629a474632899d
John Kerr <kerr.john@gmail.com>
2019-03-27 22:16:57 +0000

    gh-31 Homepage static content loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,7 +123,8 @@ class IndexPage extends React.Component {
                 <ul>
                   <li className="automation">
                     <h3>End to End Automation</h3>
-                    <p>No more spreadsheets, paper forms and emails. Less mistakes and full audit trails so you can focus
+                    <p>No more spreadsheets, paper forms and emails. Less mistakes and full audit trails so you can
+                      focus
                       on growing your business.</p>
                   </li>
                   <li className="flexibility">
@@ -155,20 +156,21 @@ class IndexPage extends React.Component {
             <ul>
               <li className="auto-calc">
                 <h3>Auto Calculation</h3>
-                <p>Automatically calculate all your commission payments and allocate them to the right payroll
-                  periods.</p>
+                <p>Konquest automatically calculates all of your commission payments and allocates them to the right
+                  payroll periods.</p>
               </li>
               <li className="central-data">
                 <h3>Centralised Data</h3>
-                <p>Organise commission and bonus related data in one, cloud based solution.</p>
+                <p>Organise all of your commission and bonus related data in one, cloud based solution.</p>
               </li>
               <li className="secure-auto">
                 <h3>Secure Automation</h3>
-                <p>End to end, securely encrypted cloud solution. No more unsecure emails or corrupted spreadsheets.</p>
+                <p>No more insecure emails or corrupted spreadsheets. Konquest offers a securely encrypted, end to end
+                  solution in the cloud.</p>
               </li>
               <li className="crm">
                 <h3>CRM Integration</h3>
-                <p>Securely integrate with your CRM via API to push sales data into Konquest</p>
+                <p>Securely integrate with your CRM via API to push sales data into Konquest.</p>
               </li>
             </ul>
             <OpenContactButton>Request Demo</OpenContactButton>
@@ -176,7 +178,8 @@ class IndexPage extends React.Component {
           <PersonaSection className="finance" isShown={this.isSectionOpen('Finance')}>
             <div className="description">
               <h2>Donec lacinia meat lover</h2>
-              <p>Remove spreadsheets from your commissions process with a secure, automated solution in the cloud.</p>
+              <p>Minimize errors and improve efficiency by removing manual input from your commission claim process. All
+                the while ensuring every transaction is recorded in a transparent audit trail.</p>
               <FinanceImage/>
             </div>
             <div className="details">
@@ -191,7 +194,7 @@ class IndexPage extends React.Component {
                 </li>
                 <li className="forecasting">
                   <h3>Improved Forecasting</h3>
-                  <p>Realtime insight into estimated and actual commissions payable in any given period</p>
+                  <p>Realtime insight into estimated and actual commissions payable in any given period.</p>
                 </li>
                 <li className="productivity">
                   <h3>Increased Productivity</h3>
@@ -203,14 +206,15 @@ class IndexPage extends React.Component {
           </PersonaSection>
           <PersonaSection className="sales" isShown={this.isSectionOpen('Sales')}>
             <div className="details">
-              <h2>Total Transparency for your Reward Sytem</h2>
+              <h2>Total Transparency for your Reward System</h2>
               <p>Understand how much you are earning before you earn it, automatically compile commission claims and be
                 involved in the approvals process so there’s never a nasty surprise on pay day.</p>
               <ul>
                 <li className="motivation">
                   <h3>Motivation</h3>
                   <p>One click access to commission pipelines for every consultant, real-time tracking against
-                    organisational or personal targets with notifications </p>
+                    organizational or personal targets with notifications [will move to a gamified experience in a near
+                    future release].</p>
                 </li>
                 <li className="focus">
                   <h3>Focus</h3>
@@ -275,27 +279,38 @@ class IndexPage extends React.Component {
           <ul className="benefits-list">
             <li>
               <h3>Drive Efficiency</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla
-                volutpat eget. Duis non massa iaculis, euismod nunc quis, egestas metus. Curabitur ac leo a dui
-                vehicula blandit. Donec a purus id ex tincidunt pulvinar. Proin a eleifend metus.</p>
+              <p>Konquest dramatically reduces the amount of time and effort your organisation needs to put into
+                managing commission, saving you substantial amounts of time every month – time that could and should be
+                spent driving revenue and profitability.</p>
+              <p>By integrating with your CRM, or using a Konquest custom interface through which to record deal data,
+                the process from calculation to approval can be almost entirely automated. Anything that can’t be
+                automatically calculated, such as guaranteed bonus for new starters, or deductions such as clawbacks,
+                can be added in a couple of clicks.</p>
+              <p>The end result is a dramatic reduction in manual input, not to mention monthly headaches!</p>
             </li>
             <li>
-              <h3>Drive Efficiency</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla
-                volutpat eget. Duis non massa iaculis, euismod nunc quis, egestas metus. Curabitur ac leo a dui
-                vehicula blandit. Donec a purus id ex tincidunt pulvinar. Proin a eleifend metus.</p>
+              <h3>Increased Engagement</h3>
+              <p>Konquest brings total transparency to your rewards program by ensuring that every member of your team
+                knows exactly how your schemes work and what that means to them for any given period in real time.</p>
+              <p>Estimated and secured earnings are visible for all, and any time something changes the effected team
+                member is notified which helps alleviate any nasty surprises come pay day, as well as giving them an
+                opportunity to take any necessary or expected actions.</p>
+              <p>The result is an informed and engaged team who trust you and your process.</p>
             </li>
             <li>
-              <h3>Drive Efficiency</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla
-                volutpat eget. Duis non massa iaculis, euismod nunc quis, egestas metus. Curabitur ac leo a dui
-                vehicula blandit. Donec a purus id ex tincidunt pulvinar. Proin a eleifend metus.</p>
+              <h3>Increased Performance</h3>
+              <p>Konquest shows your team their estimated and secured earnings for current and future months.
+                Associating their efforts to their personal outcome so clearly is a powerful motivator – who doesn’t
+                want to see their commission pot increase?!</p>
+              <p>Add in progress indicators against organisational or personal targets and you can expect a real
+                increase in performance company wide.</p>
             </li>
             <li>
-              <h3>Drive Efficiency</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium orci magna, eu rutrum nulla
-                volutpat eget. Duis non massa iaculis, euismod nunc quis, egestas metus. Curabitur ac leo a dui
-                vehicula blandit. Donec a purus id ex tincidunt pulvinar. Proin a eleifend metus.</p>
+              <h3>Ensure Accuracy and Accountability</h3>
+              <p>Konquest automatically calculates everything for you based on your unique commission schemes. This
+                means the chances of an error are greatly reduced, and because we record every event associated with
+                claims, each stakeholder is accountable to their own actions. Be that the deal data entered by
+                consultants, an approval actioned by a manager, or a clawback added by your FD.</p>
             </li>
           </ul>
         </section>
