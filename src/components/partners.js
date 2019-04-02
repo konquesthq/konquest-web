a8fabc023d5ca9bf3fb477af49f94821e244ff3d
John Kerr <kerr.john@gmail.com>
2019-04-02 23:10:55 +0100

    gh-31 Launch partner logos

--- src/components/partners.js
+++ src/components/partners.js
@@ -1,42 +1,68 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import {StaticQuery, graphql} from "gatsby";
 import Img from "gatsby-image";
 
 import './partners.scss';
 
-const PartnerLogo = () => (
+const PartnerLogo = ({partnerImage}) => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "konquest_logo.png" }) {
+        partnerOneImage: file(relativePath: { eq: "logos/bullhorn.png" }) {
           childImageSharp {
-            fluid(maxWidth: 163) {
+            fluid(maxWidth: 234) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        },
+        partnerTwoImage: file(relativePath: { eq: "logos/accessgroup.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 234, maxHeight: 150) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        },
+        partnerThreeImage: file(relativePath: { eq: "logos/engage.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 234) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        },
+        partnerFourImage: file(relativePath: { eq: "logos/vincere.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 234) {
               ...GatsbyImageSharpFluid
             }
           }
         }
       }
     `}
-    render={data => <Img className="img" fluid={data.placeholderImage.childImageSharp.fluid}/>}
+    render={data => <Img className="img" fluid={data[partnerImage].childImageSharp.fluid}/>}
   />
 );
+
+PartnerLogo.propTypes = {
+  partnerImage: PropTypes.string.isRequired
+};
 
 const Partners = () => (
   <ul className="partners">
     <li>
-      <PartnerLogo/>
+      <PartnerLogo partnerImage="partnerOneImage"/>
       <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
     </li>
     <li>
-      <PartnerLogo/>
+      <PartnerLogo partnerImage="partnerTwoImage"/>
       <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
     </li>
     <li>
-      <PartnerLogo/>
+      <PartnerLogo partnerImage="partnerThreeImage"/>
       <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
     </li>
     <li>
-      <PartnerLogo/>
+      <PartnerLogo partnerImage="partnerFourImage"/>
       <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
     </li>
   </ul>
