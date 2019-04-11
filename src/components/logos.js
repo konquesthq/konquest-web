554d0b5c8a4ebc50962ab2eb042d7dc6a9258b70
John Kerr <kerr.john@gmail.com>
2019-04-11 22:15:36 +0100

    Partners CMS integration, tidy up logos and partners

--- src/components/logos.js
+++ src/components/logos.js
@@ -1,15 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
 import {StaticQuery, graphql} from "gatsby";
 import Img from "gatsby-image";
 
 import './logos.scss';
 
-const Logo = ({customerImage}) => (
+const Logos = () => (
   <StaticQuery
     query={graphql`
       query {
-        contentfulLogos	{
+        contentfulLogos(title: { eq: "Customer Logos" })	{
           logos {
             id,
             fluid(maxHeight: 112) {
@@ -19,22 +18,18 @@ const Logo = ({customerImage}) => (
         }
       }
     `}
-    render={data => <Img className="img" fluid={data.contentfulLogos.logos[customerImage].fluid}/>}
+    render={data => (
+      <ul className="logos">
+        {
+          data.contentfulLogos.logos.map(node => (
+            <li>
+              <Img className="img" fluid={node.fluid}/>
+            </li>
+          ))
+        }
+      </ul>
+    )}
   />
-);
-
-Logo.propTypes = {
-  customerImage: PropTypes.number.isRequired
-};
-
-const Logos = () => (
-  <ul className="logos">
-    <li><Logo customerImage={0}/></li>
-    <li><Logo customerImage={1}/></li>
-    <li><Logo customerImage={2}/></li>
-    <li><Logo customerImage={3}/></li>
-    <li><Logo customerImage={4}/></li>
-  </ul>
 );
 
 export default Logos;
--- src/components/partners.js
+++ src/components/partners.js
@@ -1,67 +1,36 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import {StaticQuery, graphql} from "gatsby";
 import Img from "gatsby-image";
 
 import './partners.scss';
 
-const PartnerLogo = ({partnerImage}) => (
+const Partners = () => (
   <StaticQuery
     query={graphql`
       query {
-        partnerOneImage: file(relativePath: { eq: "logos/partner-bullhorn.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 234) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        },
-        partnerTwoImage: file(relativePath: { eq: "logos/partner-accessgroup.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 234) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        },
-        partnerThreeImage: file(relativePath: { eq: "logos/partner-engage.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 234) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        },
-        partnerFourImage: file(relativePath: { eq: "logos/partner-vincere.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 234) {
-              ...GatsbyImageSharpFluid
+        contentfulLogos(title: { eq: "Partner Logos" })	{
+          logos {
+            id,
+            fluid(maxHeight: 234) {
+              ...GatsbyContentfulFluid
             }
           }
         }
       }
     `}
-    render={data => <Img className="img" fluid={data[partnerImage].childImageSharp.fluid}/>}
+    render={data => (
+        <ul className="partners">
+          {
+            data.contentfulLogos.logos.map(node => (
+              <li>
+                <Img className="img" fluid={node.fluid}/>
+              </li>
+            ))
+          }
+        </ul>
+      )
+    }
   />
-);
-
-PartnerLogo.propTypes = {
-  partnerImage: PropTypes.string.isRequired
-};
-
-const Partners = () => (
-  <ul className="partners">
-    <li>
-      <PartnerLogo partnerImage="partnerOneImage"/>
-    </li>
-    <li>
-      <PartnerLogo partnerImage="partnerTwoImage"/>
-    </li>
-    <li>
-      <PartnerLogo partnerImage="partnerThreeImage"/>
-    </li>
-    <li>
-      <PartnerLogo partnerImage="partnerFourImage"/>
-    </li>
-  </ul>
 );
 
 export default Partners;
