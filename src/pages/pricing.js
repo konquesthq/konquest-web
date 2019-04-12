970e8ac738442b46bf87c27c5ac27f007d1522ef
John Kerr <kerr.john@gmail.com>
2019-04-12 17:44:49 +0100

    gh-24 Pricing page CMS integration

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -4,25 +4,23 @@ import SEO from "../components/seo";
 import Logos from "../components/logos";
 import RequestDemo from "../components/requestDemo";
 import OpenContactButton from '../components/openContactButton';
+import {graphql} from "gatsby";
 
 import './pricing.scss';
 
-const PricingPage = () => (
+const PricingPage = ({data}) => (
   <Layout>
     <SEO title="Pricing" keywords={[`konquest`, `commission`, `sales`, `recruitment`, `pricing`]}/>
     <h1>Pricing</h1>
     <div className="pricing-overview">
       <section className="plan-details">
         <h2>Plan Details</h2>
-        <p><span className="price">£10</span> user/month</p>
+        <p><span className="price">{data.pricing.price}</span>&nbsp;{data.pricing.priceTerm}</p>
         <div className="features">
           <ul>
-            <li>Unlimited bonus & commission schemes</li>
-            <li>CRM and Timesheet Integration or</li>
-            <li>Custom interfaces</li>
-            <li>Custom approval process</li>
-            <li>Individual dashboard</li>
-            <li>Support by humans</li>
+            {data.pricing.features.map(feature => (
+              <li>{feature}</li>
+            ))}
           </ul>
           <OpenContactButton>Request a Demo</OpenContactButton>
         </div>
@@ -31,21 +29,16 @@ const PricingPage = () => (
         <h2>FAQs</h2>
         <div className="faq-items">
           <dl>
-            <dt>Do I need to commit to a lengthy contract?</dt>
-            <dd>Nope. Konquest is available to all customers on a rolling monthly basis. If you'd prefer to pay for upfront for a longer period in exchange for a discount then we'd be delighted to discuss this with you.
-            </dd>
-            <dt>Why is there a setup fee?</dt>
-            <dd>We'd really rather not charge this, but the reality is that there is a lot of effort required from us to get you setup. This process includes the calibration of all of your commission schemes, as well as your CRM/timesheet integration(s)*
-            </dd>
-            <dt>My commission scheme is complex, are you sure you can accommodate?</dt>
-            <dd>Whilst we can't guarantee we can accommodate every scheme on Earth, we've designed Konquest to be incredibly
-              flexible. As a rule, as long as the data needed for any calculation is available, we can help you. So far, we're yet to run into a scenario we can't handle.
-            </dd>
-            <dt>Can I have a free trial?</dt>
-            <dd>Sadly not, we're sorry. The setup process takes too much time for us to do it for free. Your minimum commitment would be our one off setup cost plus the total cost of 1 month's subscription.
-            </dd>
+            {data.faqs.edges.map(({node}) => (
+              <React.Fragment>
+                <dt>{node.question.question}</dt>
+                <dd>{node.answer.answer}</dd>
+              </React.Fragment>
+            ))}
           </dl>
-          <p className="caveat">* The standard one off setup fee includes integration with any of our existing integration partners. Should you use a different system, please reach out to us so we can discuss integration with you on an individual basis. Our policy is to never charge where it is avoidable.</p>
+          {data.pricing.caveat ? (
+            <p className="caveat">* The standard one off setup fee includes integration with any of our existing integration partners. Should you use a different system, please reach out to us so we can discuss integration with you on an individual basis. Our policy is to never charge where it is avoidable.</p>
+          ) : ''}
         </div>
       </section>
     </div>
@@ -56,8 +49,8 @@ const PricingPage = () => (
       </section>
       <section className="quote">
         <blockquote>
-          Konquest removed a headache we go through every month with our commissions, but for me the real win is putting our consultants earnings right in front of them, they love that.
-          <cite>Mark Bracknall, <span className="company">Director, Theo James</span></cite>
+          {data.quote.quote.quote}
+          <cite>{data.quote.citationPerson}, <span className="company">{data.quote.citationCompany}</span></cite>
         </blockquote>
       </section>
     </div>
@@ -66,3 +59,36 @@ const PricingPage = () => (
 );
 
 export default PricingPage;
+
+export const pricingQuery = graphql`
+  query {
+    pricing: contentfulPricing(title: { eq: "Pricing" }) {
+      price,
+      priceTerm,
+      features,
+      ctaText,
+      caveat {
+        caveat
+      }
+    }
+    faqs: allContentfulFaq {
+      edges {
+        node {
+          question {
+            question
+          },
+          answer {
+            answer
+          }
+        }
+      }
+    }
+    quote: contentfulQuote(title: { eq: "Quote" }) {
+      quote {
+        quote
+      },
+      citationPerson,
+      citationCompany
+    }
+  }
+`;
