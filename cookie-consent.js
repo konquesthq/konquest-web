42be3c5cc6ce63d16e0c5119f7e389f545f39c41
John Kerr <kerr.john@gmail.com>
2019-04-17 22:55:20 +0100

    gh-27 Added cookie consent plugin

---
+++ cookie-consent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const headConsent = () => (
+  <script key="cookie-consent-head" id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="84e3af6c-2a1d-48a9-ac63-25a30d582f99" type="text/javascript" async />
+);
+
+export const postBodyConsent = () => (
+  <script key="cookie-consent-post-body" id="CookieDeclaration" src="https://consent.cookiebot.com/84e3af6c-2a1d-48a9-ac63-25a30d582f99/cd.js" type="text/javascript" async />
+);
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,8 @@
 import wrapWithProvider from './providerWrapper';
+import {headConsent, postBodyConsent} from './cookie-consent';
+
 export const wrapRootElement = wrapWithProvider;
+export const onRenderBody = ({setHeadComponents, setPostBodyComponents}) => {
+  setHeadComponents([headConsent()]);
+  setPostBodyComponents([postBodyConsent()]);
+};
